b1a79205104a36a2185774b16a2698f50e52c91a
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2023-01-13 20:39:33 -0700

    redeploy and updates to the quote stuff

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,13 +1,13 @@
-interface QuoteProps {
+export interface IQuote {
   body: string;
   author: string;
 }
 
-export const Quote = ({ body, author }: QuoteProps) => {
+export const Quote = ({ body, author }: IQuote) => {
   return (
     <div>
       <p> {body}</p>
-      <p style={{ textAlign: "right" }}> {author} </p>
+      <p style={{ textAlign: "right" }}> -{author} </p>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
-import { Quote } from "../components/Quote";
+import { useEffect, useState } from "react";
+import { Quote, IQuote } from "../components/Quote";
 import { SearchBar } from "../components/Search";
 
 import "./HomePage.css";
-
 export function HomePage() {
   const [searchState, setSearchState] = useState(false);
+  const [quotes, setQuotes] = useState<IQuote[]>([]);
+
+  async function updateQuote() {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+
+    if (response.ok) {
+      // Update DOM elements
+      console.log(data);
+      quotes.push({
+        author: data.author,
+        body: data.content,
+      });
+    } else {
+      quotes.push({
+        author: "Error",
+        body: "Cannot get a quote at this time",
+      });
+      console.log(data);
+      console.log("Error");
+    }
+  }
+
+  useEffect(() => {
+    updateQuote();
+  });
 
   return (
     <div>
@@ -16,7 +42,11 @@ export function HomePage() {
           placeholder="Search"
         ></input>
       </div>
-      <Quote body="tmp" author="tmp" />
+      <div>
+        {quotes.map(({ body, author }: IQuote) => {
+          return <Quote body={body} author={author} />;
+        })}
+      </div>
     </div>
   );
 }
